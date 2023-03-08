d7a4ecc27d6d7ce67f8ffd3a947b95e43e5f586a
zahra <zahraaahmadi141997@email.com>
2023-03-08 00:41:46 -0800

    create product table with react bootstrap table

---
+++ src/Components/ProductTable/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styles from "./style.module.css"
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ProductTable = () => {
+  return (
+    
+    <div className={styles.table}>
+    <Table striped size="sm" bordered>
+      <thead >
+        <tr>
+        <th >تصویر</th>
+          <th>نام کالا</th>
+          <th >دسته بندی</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </Table>
+
+
+    </div>
+  )
+}
+export default ProductTable;
--- src/Pages/ProductPanel/index.js
+++ src/Pages/ProductPanel/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ProductTable from '../../Components/ProductTable/index.js';
 import  AdminHeader  from '../../Layouts/AdminHeader/index.js';
+import Button from '../../Components/button';
  const ProductPanel = () => {
   return (
     <>
     <AdminHeader/>
-    <div>ProductPanel</div>
+    <Button btnColor={"blue"}>{"افزودن کالا"}</Button>
+    <ProductTable/>
     </>
     
   )
