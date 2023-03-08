3739c9b8a38e29244eaad79f80842fed749db9ab
zahra <zahraaahmadi141997@email.com>
2023-03-08 01:43:16 -0800

    edit styles of product panel page

--- src/Pages/ProductPanel/index.js
+++ src/Pages/ProductPanel/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import styles from "./style.module.css"
 import ProductTable from '../../Components/ProductTable/index.js';
 import  AdminHeader  from '../../Layouts/AdminHeader/index.js';
 import Button from '../../Components/button';
  const ProductPanel = () => {
+
+
   return (
     <>
     <AdminHeader/>
-    <Button btnColor={"blue"}>{"افزودن کالا"}</Button>
+    <div className={styles.add}>
+    <Button btnColor={"rgb(7 68 199)"}>{"افزودن کالا"}</Button>
+    </div>
+   
     <ProductTable/>
     </>
     
