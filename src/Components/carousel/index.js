1580d0519f29e88249374c23dceb378f403f8a1e
zahra <zahraaahmadi141997@email.com>
2023-03-24 05:11:46 -0700

    create carousel component

---
+++ src/Components/carousel/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import pic1 from "../../../src/Assets/images/carousel1.webp"
+import pic2 from "../../../src/Assets/images/carousel2.gif"
+import pic3 from "../../../src/Assets/images/carousel3.jpg"
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+         className="d-block w-100"
+          src={pic1}
+        />
+      
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={pic2}
+         />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={pic3}
+        />
+
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+export default ControlledCarousel;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,19 +2,21 @@ import React from 'react'
 import styles from "./style.module.css"
 import { Link } from 'react-router-dom';
 import Header from '../../Layouts/header';
+import ControlledCarousel from '../../Components/Carousel';
+
  const Home = () => {
   return (
     <>
       <Header/>
-    <div className={styles.sliderWraper}>
-
-    </div>
+   
      <div className={styles.products}>
        <Link to="/Products">گوشی موبایل</Link>
        <Link to="/Products">تبلت</Link>
        <Link to="/Products">لپ تاپ</Link>
        <Link to="/Products">ایرپاد</Link>
      </div>
+
+   <ControlledCarousel/>
     </>
   )
 }
