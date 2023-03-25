eae89488cb78166fa86c03a8bef63865282c741a
zahra <zahraaahmadi141997@email.com>
2023-03-25 09:47:19 -0700

    create card component for products

---
+++ src/Components/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./style.module.css"
+ const card = () => {
+  return (
+    <div className={styles.wrapperCard}>
+     <img/>
+     <p></p>
+    </div>
+  )
+}
+export default card;
