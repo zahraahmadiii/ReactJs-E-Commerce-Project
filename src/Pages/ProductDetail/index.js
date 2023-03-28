1eddfdb996a32fe828d1b985cdc8b67edc62a384
zahra <zahraaahmadi141997@email.com>
2023-03-28 13:41:33 -0700

    create productdetail page to show product

--- src/Pages/ProductDetail/index.js
+++ src/Pages/ProductDetail/index.js
@@ -7,10 +7,24 @@ const ProductDetail = () => {
   return (
     <>
      <Header/>
-     <div className={styles.ProductDetail}>
-       <Link to="/Products/ProductDetail"><Button>{"افزودن به سبد خرید"}</Button></Link>
-       <h2>کالای فلان</h2>
+     <div className={styles.ProductDetail} >
+      <div className={styles.wraperBtn}>
+      <div className={styles.wraperAllData}>
+         <p className={styles.name}>نام کالا:</p>
+         <p className={styles.brand}>برند کالا:</p>
+         <p className={styles.price}>قیمت:</p>
+         <p className={styles.desc}>توضیحات:</p>
+        </div>
+        <Link to="/Products/ProductDetail"><Button btnColor={" rgb(7 68 199)"}>{"افزودن به سبد خرید"}</Button></Link>
+
+      </div>
+      
+        <div className={styles.wraperImage}>
+          <img  className={styles.Image}/>
+        </div>
+       
      </div>
+    
     </>
   
   )
