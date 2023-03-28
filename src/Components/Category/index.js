d0b086754facec658b5561d75edb62faf87ee0c4
zahra <zahraaahmadi141997@email.com>
2023-03-28 02:28:53 -0700

    edit category component

--- src/Components/Category/index.js
+++ src/Components/Category/index.js
@@ -4,10 +4,11 @@ import Card from '../Card'
 import styles from "./style.module.css"
 
 const getProduct=async(str)=>{
-
+    console.log(str)
     const res= await getProductsCategory(str,1,6)
     return res.data
 }
+
 export const Category = ({str}) => {
 
 const [products,setProducts]=useState([])
@@ -15,14 +16,12 @@ const [products,setProducts]=useState([])
 useEffect(()=>{
     getProduct(str).then(res => setProducts(res))
    },[])
+
    console.log(products)
+
   return (
-   
-  
      <div className={styles.wraper_products}>
       {products.map((item)=> <Card key={item.id} item={item}/>)} 
      </div>
- 
-
   )
 }
