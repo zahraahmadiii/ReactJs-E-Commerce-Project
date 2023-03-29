7dc7fe7b489f9fc061cea70e15f029a7df010b5f
zahra <zahraaahmadi141997@email.com>
2023-03-29 12:08:29 -0700

    authunticate login panel ,get token and save in local storage

--- src/Api/Constants/index.js
+++ src/Api/Constants/index.js
@@ -6,35 +6,6 @@ export const instance=axios.create({
 })
 
 
-// instance.defaults.headers.token = Cookies.get('token');
 
-instance.interceptors.request.use(
-    config => {
-        const authToken = localStorage.getItem('token');
-        if (authToken !== undefined) {
-            config.headers.token = authToken;
-            config.timeout = 800000;
-        }
-        return config;
-    }, (err) => {
-        return Promise.reject(err);
-    }
-);
 
-instance.interceptors.response.use(response => {
-    return response;
-}, error => {
-    const { response: { status } } = error;
 
-    if (status === 401) {
-        instance.defaults.headers.refreshToken = localStorage.getItem('refresh_token');
-        instance.post('/auth/refresh-token').then(({ data }) => {
-            console.log(data)
-            const { accessToken } = data;
-            localStorage.setItem('token', accessToken);
-            window.location.reload()
-        })
-    } else {
-        return Promise.reject(error);
-    }
-});
--- src/Pages/AdminPanel/index.js
+++ src/Pages/AdminPanel/index.js
@@ -5,17 +5,23 @@ import OrderTable from "../../Components/OrderTable/index.js";
 import {useDispatch, useSelector} from 'react-redux'
 import { getOrder, setDelivery } from '../../redux/Features/orders-slice.js';
 import OrderModal from "../../Components/Order-modal"
+import { Navigate } from "react-router-dom";
  const AdminPanel = () => {
 
   const orders = useSelector(store => store.ordersSlice);
-  // console.log(orders)
+ 
 
    const dispatch=useDispatch()
    useEffect(()=>{
     dispatch(getOrder({delivery:orders.delivery}))
    },[orders.delivery])
 
-   
+   useEffect(() => {
+     const token=localStorage.getItem("token")
+   if(!token){
+    Navigate("/LoginPanel")
+  }
+   },[])
 
   return (
     <>
@@ -31,9 +37,7 @@ import OrderModal from "../../Components/Order-modal"
         <input  type="radio" className={styles.check} name="deliverystatus" checked={orders.delivery} onChange={()=>dispatch(setDelivery({delivery:true}))}/>
         <label className={styles.label}> سفارش های تحویل شده</label>
         </div>
-       
         </div>
- 
         <OrderTable item={orders.data}/>
     </div>
     </div>
